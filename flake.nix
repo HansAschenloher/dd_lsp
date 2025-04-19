3007f0f396086cba82129517bfefb193756a7465
Johann Aschenloher <hansi.aschenloher@gmail.com>
2025-04-19 21:16:47 +0200

    Add nixvim

--- flake.nix
+++ flake.nix
@@ -1,14 +1,32 @@
 {
   description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      nixvim,
+      home-manager,
+    }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (final: prev: { dd_lsp = self.packages.x86_64-linux.default; }) ];
+      };
     in
     {
       packages.x86_64-linux.default = pkgs.haskellPackages.developPackage {
@@ -23,8 +41,29 @@
         inputsFrom = [ self.packages.x86_64-linux.default ];
       };
 
+      homeConfigurations."ja@pc" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [
+          {
+            home.stateVersion = "24.11";
+            home.username = "ja";
+            home.homeDirectory = "/home/ja";
+            programs.home-manager.enable = true;
+            programs.git.enable = true;
+            programs.zsh.enable = true;
+          }
+          nixvim.homeManagerModules.nixvim
+          ./nixvim.nix
+        ];
+      };
+
+      overlays.default = final: prev: { dd_lsp = self.packages.x86_64-linux.default; };
+
       homeManagerModules.nixvim = {
-        imports = [ ./nixvim.nix ];
+        imports = [
+          ./nixvim.nix
+          #nixvim.homeManagerModules.nixvim
+        ];
       };
     };
 }
--- nixvim.nix
+++ nixvim.nix
@@ -6,15 +6,44 @@
 }:
 let
   cfg = config.plugins.lsp.servers.dd_lsp;
-  mkLsp = import ./mklsp.nix;
-  inherit (lib) types;
 in
 {
-  imports = [
-    (mkLsp {
-      name = "dd_lsp";
-      package = pkgs.callPackage ./default.nix;
-      cmd = "${pkgs.hello}/bin/hello";
-    })
-  ];
+
+  options.plugins.lsp.servers.dd_lsp = {
+    enable = lib.mkEnableOption "the DocDir lsp";
+  };
+
+  #config = lib.mkIf cfg.enable {
+  config = {
+    programs.nixvim.extraConfigLuaPost = ''
+      local lspconfig = require('lspconfig')
+      local configs = require('lspconfig.configs')
+
+      -- Register the custom LSP server if it's not already defined
+      if not configs.dd_lsp then
+        configs.dd_lsp = {
+          default_config = {
+            cmd = { "${pkgs.dd_lsp}/bin/dd_lsp" },
+            filetypes = { "*" },  -- Enable for all filetypes
+            root_dir = function(fname)
+              return lspconfig.util.root_pattern(".git", "dd.config")(fname) or vim.fn.getcwd()
+            end,
+            settings = {},
+          },
+        }
+      end
+
+      -- Set up the server
+      lspconfig.dd_lsp.setup({})
+    '';
+    programs.nixvim.plugins.lsp.enabledServers = [
+      {
+        name = "dd_lsp";
+        extraOptions = {
+          cmd = "${pkgs.hello}/bin/hello";
+          autostart = true;
+        };
+      }
+    ];
+  };
 }
